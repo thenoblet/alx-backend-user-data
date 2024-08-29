b56fff3724cdb30d74025682d9d5d52c962c59f6
noblet <appiahpatricknoblet@gmail.com>
2024-08-29 22:24:38 +0100

    Type annotated get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,7 +22,7 @@ import re
 import os
 import logging
 from typing import List
-from mysql.connector import errors
+import mysql.connector
 from mysql.connector.connection import MySQLConnection
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -79,17 +79,31 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> MySQLConnection:
-    """Return a database connection."""
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Establishes a connection to a MySQL database using credentials from
+    environment variables.
+
+    The function attempts to connect to a MySQL database using the following
+    environment variables: 'PERSONAL_DATA_DB_USERNAME',
+    'PERSONAL_DATA_DB_PASSWORD', 'PERSONAL_DATA_DB_HOST', and
+    'PERSONAL_DATA_DB_NAME'. If any errors occur during the connection attempt,
+    an error message is logged.
+
+    Returns:
+        MySQLConnection: A MySQL database connection object if successful,
+        otherwise None.
+    """
     try:
-        return MySQLConnection(
-            user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
-            password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
-            host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
-            database=os.environ.get("PERSONAL_DATA_DB_NAME"),
+        conn = mysql.connector.connect(
+            user=os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root'),
+            password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ""),
+            host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+            database=os.environ.get('PERSONAL_DATA_DB_NAME', ""),
         )
-    except errors.ProgrammingError as e:
-        logging.error(f"Database connection failed - {e.msg}")
+        return conn
+    except mysql.connector.Error as e:
+        return None
 
 
 def main():
