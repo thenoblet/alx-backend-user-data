2e81b837b82f7c3dbc9adba53317ace9cee5a65d
noblet <appiahpatricknoblet@gmail.com>
2024-08-30 00:25:52 +0100

    Added a function that Validates a password against a hashed password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 """
-Password Hashing Utility
+Password Hashing and Validation Utility
 
-This script provides a utility function to hash passwords using the
-bcrypt algorithm. It ensures that passwords are securely hashed before
-being stored or transmitted.
+This script provides utility functions to hash passwords and verify
+hashed passwords using the bcrypt algorithm. It ensures that passwords
+are securely hashed before being stored or transmitted and allows for
+the validation of passwords against their hashes.
 
-The `hash_password` function takes a plain text password, hashes it
-using bcrypt, and returns the hashed password.
+The `hash_password` function hashes a plain text password using bcrypt.
 
+The `is_valid` function checks if a given plain text password matches
+a hashed password.
 """
 
 import bcrypt
@@ -31,3 +33,22 @@ def hash_password(password: str) -> bytes:
     """
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates a password against a hashed password.
+
+    This function takes a hashed password and a plain text password,
+    encodes the plain text password to bytes, and uses `bcrypt.checkpw()`
+    to check if the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password to validate against.
+        password (str): The plain text password to check.
+
+    Returns:
+        bool: True if the password matches the hashed password,
+        False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
