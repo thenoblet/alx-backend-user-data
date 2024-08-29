967f88a43d47f6bee162e252c90ba3f027bd60b4
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-08-29 17:52:00 +0000

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,7 +34,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: List):
         """Initialize logger."""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
