afc82cbb41ab5ce73f9b36224c12041129e58987
noblet <appiahpatricknoblet@gmail.com>
2024-08-29 20:01:12 +0100

    ADDED: 'get_logger' method - sets up and returns a logger configured to redact sensitive information.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,11 +10,19 @@ The `filter_datum` function takes a list of fields to redact, a redaction
 string, a message, and a separator. It replaces the values associated
 with the specified fields in the message with the redaction string.
 
+The `RedactingFormatter` class extends the `logging.Formatter` class to
+redact specified fields in log messages.
+
+The `get_logger` function sets up a logger with the `RedactingFormatter`
+to ensure sensitive information is not logged.
+
 """
 
 import re
 import logging
 from typing import List
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
@@ -44,3 +52,25 @@ class RedactingFormatter(logging.Formatter):
         log_message = super(RedactingFormatter, self).format(record)
         return filter_datum(
             self.fields, self.REDACTION, log_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Sets up and returns a logger configured to redact sensitive information.
+
+    The logger is set to the INFO level and uses a stream handler with the
+    `RedactingFormatter` to ensure that any log messages containing sensitive
+    information are redacted before being output.
+
+    Returns:
+        logging.Logger: A configured logger instance.
+    """
+    logger = logging.getLogger(name='user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(fields=list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
