63e8650f2758900a719b846250024480e8586da6
noblet <appiahpatricknoblet@gmail.com>
2024-08-29 18:48:43 +0100

    ADDED: Redacting Formatter class to redact sensitive information in log messages.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,13 +34,13 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List):
+    def __init__(self, fields: List[str]):
         """Initialize logger."""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(fmt=self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Format the log record, redacting sensitive information."""
-        log_message = super(RedactingFormatter, self).format(record)
+        log_message = super(RedactingFormatter, self).format(record=record)
         return filter_datum(
             self.fields, self.REDACTION, log_message, self.SEPARATOR)
