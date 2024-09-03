046c6c2c924d43ab2d36d8e7118d613e08efdd18
noblet <appiahpatricknoblet@gmail.com>
2024-09-03 20:03:26 +0100

    ADDED: added a module that provides the  class for managing API authentication.

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+"""
+API Authentication Module
+
+This module provides the `Auth` class for managing API authentication.
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """
+    Auth class to manage API authentication.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if the given path requires authentication.
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Gets the value of the Authorization header from the request.
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Gets the current user based on the request.
+        """
+        return None
